076a133334fafc14f7b0e1a74366f6ef7362f73f
Dennis Ploeger <develop@dieploegers.de>
2022-04-20 09:09:26 +0200

    feat: Support 2022.1

--- src/main/java/de/dieploegers/develop/idea/shellfilter/CustomFilterAction.java
+++ src/main/java/de/dieploegers/develop/idea/shellfilter/CustomFilterAction.java
@@ -4,22 +4,14 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.Pair;
 import de.dieploegers.develop.idea.shellfilter.beans.CommandBean;
-import icons.ShellFilterIcons;
-import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NonNls;
-
-import java.util.ResourceBundle;
 
 public class CustomFilterAction extends FilterAction {
 
     @NonNls
     private final Logger LOG = Logger.getInstance(CustomFilterAction.class);
-    @NonNls
-    private final ResourceBundle resourceBundle =
-            ResourceBundle.getBundle("i18n.shellfilter");
 
     @Override
     public void actionPerformed(final AnActionEvent anActionEvent) {
